66eee9aef68b3bfd91e9f7d670a5767e18f368d5
craftwerk <pst@crftwrk.de>
2023-12-22 17:40:08 +0100

    Deny direct access

--- author-equal-height.php
+++ author-equal-height.php
@@ -6,10 +6,15 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Bootscore
+ * @version 5.4.0
  */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
 
 get_header();
 ?>
+
 <div id="content" class="site-content <?= bootscore_container_class(); ?> py-5 mt-5">
   <div id="primary" class="content-area">
 
